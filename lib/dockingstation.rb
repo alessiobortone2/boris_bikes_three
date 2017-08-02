75f03823a0ec4ed9a67cc4ca8735c1cb60eaeef7
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-08-02 12:18:46 +0100

    10. Making Docking Stations Release Bikes

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,11 +1,12 @@
 class DockingStation
   def release_bike
-
+    Bike.new
   end
 end
 
 class Bike
   def working?
+    true
   end
 end
 
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,4 +4,14 @@ describe DockingStation do
   it 'releases a bike' do
     expect(DockingStation.new).to respond_to(:release_bike)
   end
+
+  it 'releases a new instance of the Bike class' do
+    expect(DockingStation.new.release_bike.class).to eq Bike
+  end
+
+  it 'checks if the bike is working' do
+    expect(DockingStation.new.release_bike).to respond_to(:working?)
+  end
+
+
 end
